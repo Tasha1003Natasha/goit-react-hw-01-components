0da539182419c8e1af65ccb4d59268685c121986
Tasha1003Natasha <0955712824n@gmail.com>
2022-07-19 13:27:46 +0300

    задача 2 без стилів

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
-// import {Profile} from './Profile/Profile';
-import {ProfileList} from './Profile/ProfileList';
-import user from '../data/user.json';
+// import {ProfileList} from './Profile/ProfileList';
+// import user from '../data/user.json';
 // //////////////////2 д/з/////////////////////////////////////////
-// import data from '../data/data.json';
-// import {Statistics} from './Statistics/Statistics';
+import data from '../data/data.json';
+import {Statistics} from './Statistics/Statistics';
+// //////////////////3 д/з/////////////////////////////////////////
 
 
 export const App = () => {
@@ -18,9 +18,12 @@ export const App = () => {
         color: '#010101'
       }}
     >
-    <ProfileList user={user}/>
-    {/* <Statistics title="Upload stats" stats={data} /> */}
-     {/* <Statistics stats={data} /> */}
+     {/* <ProfileList user={user}/> */}
+
+      <Statistics title="Upload stats" stats={data} />
+
+      {/* <Statistics stats={data} /> */}
+
     </div>
   );
 };
